2d26323d795353f2210372739943556488c136ad
Jared Borders <jlbxsxs@gmail.com>
2021-04-24 01:31:19 -0500

    Implement WagerStore2 to handle Wager2 contracts

--- contracts/Wager2.sol
+++ contracts/Wager2.sol
@@ -4,16 +4,23 @@ pragma solidity ^0.8.0;
 import "./openzeppelin-solidity/contracts/access/Ownable.sol";
 import "./openzeppelin-solidity/contracts/security/Pausable.sol";
 
+/* Change Request: getRandom() may be subject to manipulation; use ChainLink VRF Oracle */
+/* Change Request: Integrate Ownable and Pausable */
+/* Change Request: Revisit how Wager.sol handles withrawls */
+
 /// @author jaredborders
 /// @title Wager2
 contract Wager2 is Pausable, Ownable {
 
     /* STATE VARIABLES */
     address public wagerer; // Wager creator
-    uint constant MIN_WAGER = 0.001 ether;
+    uint constant MIN_WAGER = 0.001 ether; // Minimum stake of wager
     uint public wagerAmount; // Amount staked by wagerer
     uint constant MIN_DURATION = 300; // Duration of wager >= 5 minutes
     uint public wagerExpireTime; // To be determined by Wagerer
+
+    // Store of all wagers 
+    address public wagerStoreAddress2;
 
     /* MODIFIERS */
     modifier validWagerAmount {
@@ -32,43 +39,56 @@ contract Wager2 is Pausable, Ownable {
     }
 
     /* CONSTRUCTOR */
-    constructor(uint _wagerDuration) 
+    /// Create a new Wager contract
+    /// @param _owner - the owner to be recognized by this contract. Not necessarily `msg.sender` nor `tx.origin`
+    /// @param _wagerDuration - The duration of the wager `open` period
+    constructor(address _owner, uint _wagerDuration) 
         validWagerAmount // Checks if Wagerer staked enough eth 
         payable 
     {
         require(_wagerDuration >= MIN_DURATION, "Duration of wager availability is too short (<5 minutes");
 
-        wagerer = msg.sender;
-        wagerAmount = uint(msg.value);
+        wagerer = _owner;
+        wagerAmount = msg.value;
         wagerExpireTime = _wagerDuration + block.timestamp; // ex. if _wagerDuration = 300 then wagerExpireTime = currentTime + 5 minutes
+
+        // Wager creation should be called from its store.
+        wagerStoreAddress2 = msg.sender; 
     }
 
     /* FUNCTIONS */
-    function challenge() 
+    /// Allow participant to withdraw money
+    /// @param _challenger - participant who wishes to accept wager proposal
+    /// @dev Checks for matching amount of eth and that wager is still active
+    function challenge(address _challenger) 
         external
         validWagerAvailability // Checks if wager is still available
         matchesWagerAmount // Checks if challenger staked same amount of eth
         payable
     {
         wagerAmount += uint(msg.value);
-        findWinner(msg.sender);
+        findWinner(_challenger);
     }
 
-    function findWinner(address challenger) 
+    /// Determine who wins
+    /// @param _challenger - participant who accepted wager proposal
+    /// @dev findWinner will pay one of the participants randomly
+    function findWinner(address _challenger) 
         internal
     {
         getRandom() == 0 ? 
             payable(wagerer).transfer(wagerAmount) : 
-            payable(challenger).transfer(wagerAmount);
+            payable(_challenger).transfer(wagerAmount);
     }
 
+    /// Generate a random value
+    /// @return 0 or 1, "randomly"
     function getRandom() 
         internal 
         view
         returns(uint) 
     {
         return uint(keccak256(abi.encodePacked(block.timestamp, block.number,block.difficulty))) % 2;
-        /* Change Request: Above code may be subject to manipulation; use ChainLink VRF Oracle */
     }
 
 }
---
+++ contracts/WagerStore2.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./openzeppelin-solidity/contracts/security/Pausable.sol";
+import "./openzeppelin-solidity/contracts/access/Ownable.sol";
+import "./Wager2.sol";
+
+/* Change Request: Integrate Ownable and Pausable */
+
+/// @author jaredborders
+/// @title WagerStore2
+contract WagerStore is Pausable, Ownable {
+
+    /* STATE VARIABLES */
+    mapping(address => bool) activeWagers;
+    address[] public wagerAddresses;
+
+    /* MODIFIERS */
+    modifier createdByThisStore(address wagerAddress) {
+        require(activeWagers[wagerAddress]); // true if address has been added to mapping
+        _;
+    }
+
+    /* CONSTRUCTOR */
+    constructor() {}
+
+    /* FUNCTIONS */
+    /// Create a new Wager
+    /// @dev The new Wager is then saved in the array of this contract for future reference
+    /// @param _duration - Duration of the wager `open` period
+    /// @return The created Wager
+    function createWagerContract(uint _duration) 
+        public 
+        payable 
+        returns(Wager2) 
+    {
+        Wager2 newWager = (new Wager2) {value: msg.value} 
+            ({
+                _owner: msg.sender,
+                _wagerDuration: _duration
+                
+            });
+
+        address wagerAddress = address(newWager);
+
+        // Update with new contract info
+        wagerAddresses.push(wagerAddress);
+        activeWagers[wagerAddress] = true;
+        return newWager;
+    }
+
+    /// Retrieve the the array of created contracts
+    /// @return An array of all created Wager contracts
+    function getWagers() 
+        public 
+        view 
+        returns(address[] memory) 
+    {
+        return wagerAddresses;
+    }
+
+}
