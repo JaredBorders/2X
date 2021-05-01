5668f7c3bd8164456c941af491d5f94d2773be51
Jared Borders <jlbxsxs@gmail.com>
2021-04-30 22:11:39 -0500

    Change Ethereum Development Framework change to Hardhat. Frontend rework

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.3",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337 // SOLVES ISSUE WITH METAMASK
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/26c1276432a44f31a1310e1cdae41034",
+      accounts: [``] // TEST ACCOUNT 1 PRIV KEY (ADD YOURS)
+    }
+  }
+};
+
